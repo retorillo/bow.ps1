faf908fc0f3f1c9ab80e93eeceea2c38220379e6
Retorillo <retorillo@outlook.com>
2016-10-09 00:15:20 +0900

    Fix bug related with arguments that contain round bracket

--- bow.ps1
+++ bow.ps1
@@ -1,14 +1,12 @@
 ﻿function convertToUbuntuPath([string]$path) {
-  $drive = [regex]'^([a-zA-Z]):'
-  $path = $drive.replace($path, {
+  return ([regex]'^([a-zA-Z]):').replace($path, {
     param($m)
     return '/mnt/' + $m.groups[1].value.tolower();
-  })
-  return $path.replace('\', '/')
+  }).replace('\', '/')
 }
 function buildUbuntuCommand([string]$command, [object[]]$commandargs) {
   return [string]::join(' ', $(
-    $s = [regex]'\s'
+    $s = [regex]'[\s()]'
     echo $command
     $commandargs | % {
       $p = convertToUbuntuPath $_
@@ -22,12 +20,22 @@ function buildUbuntuCommand([string]$command, [object[]]$commandargs) {
 function startUbuntuCommand($command, $commandargs) {
   bash -c "$(buildUbuntuCommand $command $commandargs)"
 }
+function get_title() { return $host.ui.rawui.windowtitle; }
+function set_title($value) { $host.ui.rawui.windowtitle = $value; }
 
-function start_vim() { startUbuntuCommand 'vim' $args }
+function start_x($command, $arguments) {
+ $t = get_title
+ set_title ($command + ' (bash)')
+ startUbuntuCommand $command $arguments 
+ set_title $t
+}
+function start_vim() { start_x 'vim' $args }
 set-alias vim start_vim
-function start_grep() { startUbuntuCommand 'grep' $args }
+function start_grep() { start_x 'grep' $args }
 set-alias grep start_grep
-function start_ssh() { startUbuntuCommand 'ssh' $args }
+function start_ssh() { start_x 'ssh' $args }
 set-alias ssh start_ssh
-function start_git() { startUbuntuCommand 'git' $args }
+function start_git() { start_x 'git' $args }
 set-alias git start_git
+function start_bc() { start_x 'bc' $args }
+set-alias bc start_bc
